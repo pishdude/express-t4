ea258f36d2100205ebec115e9b5a9d9fd54679b1
pish <hariarun.19@gmail.com>
2023-02-09 09:16:26 -0500

    message correction

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+var cors = require('cors');
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get("/", (req, res) => res.send("Server running!"));
 
